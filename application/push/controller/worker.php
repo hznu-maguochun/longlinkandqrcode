ae973f6338054fe040c0e64527fba1e54733082f
maguochun <maguochun@hznu.edu.cn>
2019-08-24 18:13:34 +0800

    compeleteTEST
    compeleteTEST,that is OK,Now we can do task longLink

--- application/push/controller/worker.php
+++ application/push/controller/worker.php
@@ -5,15 +5,27 @@ use think\Log;
 /**
  * Server
  */
-class worker extends Server
+class Worker extends Server
 {
 	protected $socket='websocket://localhost:2346';
 	public function OnMessage($connection,$data)
 	{
 		$connection->send("I received your message");
 	}
+	public function onConnect($connection)
+	{
+		Log::write("onConnect");
+	}
+	public function onClose($connection)
+	{
+		Log::write("onClose");
+	}
+	public function onError($connection,$code,$msg)
+	{
+		Log::write("onError");
+	}
 	public function onWorkerStart($worker)
 	{
-		Log::write($worker);
+		Log::write("start");
 	}
 }
