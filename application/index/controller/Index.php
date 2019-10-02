7c7a7e93a629c9032384b6a93856f4755c8a5094
maguochun <maguochun@163.com>
2019-10-02 17:50:17 +0800

    Update Index.php

--- application/index/controller/Index.php
+++ application/index/controller/Index.php
@@ -4,26 +4,24 @@ use think\Controller;
 use think\Request;
 use think\Log;
 
-class Index extends Controller
-{
-    public function index()
-    {
+class Index extends Controller{
+    public function index(){
         $this->assign('myurl',urlencode("http://www.163.com" ));
         return $this->fetch();
     }
-    public function getpng()
-    {
+    public function getpng(){
         $r=Request::instance();
         $myurl=$r->param('myurl');
         Log::write($myurl);
         return qrcode($myurl); 
     }
-    public function testcode()
-    {
+    public function testcode(){
     	return $this->fetch();
     }
     public function testlonglink(){
-    	
     	return $this->fetch();
+    }
+    public function getqueue(){
+        return $this->fetch();
     }
 }
